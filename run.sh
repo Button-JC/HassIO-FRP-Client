e82211cc497b9b7e3b09bb2d36246c9617964836
Button <uhlir.v+github@gmail.com>
2025-03-29 08:54:28 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -26,47 +26,47 @@ echo  "Using Encryption: ${ENCRYPTION}"
 echo  "Using Compression: ${COMPRESSION}"
 echo "*************************************"
 
-# Check if /usr/src/frpc.ini exist. If not create with template
+# Check if /usr/src/frpc.toml exist. If not create with template
 
-if [ ! -f /usr/src/frpc.ini ]; then
-    echo "Creating frpc.ini"
-    echo "[common]" > /usr/src/frpc.ini
-    echo "server_addr = ${SERVER_IP}" >> /usr/src/frpc.ini
-    echo "server_port = ${SERVER_PORT}" >> /usr/src/frpc.ini
+if [ ! -f /usr/src/frpc.toml ]; then
+    echo "Creating frpc.toml"
+    echo "[common]" > /usr/src/frpc.toml
+    echo "server_addr = ${SERVER_IP}" >> /usr/src/frpc.toml
+    echo "server_port = ${SERVER_PORT}" >> /usr/src/frpc.toml
 
     # If token is filled then add token authentication
     if [ ! -z "${AUTH_TOKEN}" ]; then
         echo "authentication_method = token" >> /usr/src/frpc.ini
-        echo "token = ${AUTH_TOKEN}" >> /usr/src/frpc.ini
+        echo "token = ${AUTH_TOKEN}" >> /usr/src/frpc.toml
     fi
 
     echo "Adding HA Exposure......."
-    echo "" >> /usr/src/frpc.ini
-    echo "[hass]" >> /usr/src/frpc.ini
-    echo "type = tcp" >> /usr/src/frpc.ini
+    echo "" >> /usr/src/frpc.toml
+    echo "[hass]" >> /usr/src/frpc.toml
+    echo "type = tcp" >> /usr/src/frpc.toml
 
     # If encryption or/and compression is enabled. add them 
     if [ "${ENCRYPTION}" = "true" ]; then
-        echo "use_encryption = true" >> /usr/src/frpc.ini
+        echo "use_encryption = true" >> /usr/src/frpc.toml
     fi
     if [ "${COMPRESSION}" = "true" ]; then
-        echo "use_compression = true" >> /usr/src/frpc.ini
+        echo "use_compression = true" >> /usr/src/frpc.toml
     fi
 
-    echo "local_ip = 127.0.0.1 " >> /usr/src/frpc.ini
-    echo "local_port = ${LOCAL_PORT}" >> /usr/src/frpc.ini
-    echo "remote_port = ${REMOTE_PORT}" >> /usr/src/frpc.ini
+    echo "local_ip = 127.0.0.1 " >> /usr/src/frpc.toml
+    echo "local_port = ${LOCAL_PORT}" >> /usr/src/frpc.toml
+    echo "remote_port = ${REMOTE_PORT}" >> /usr/src/frpc.toml
 
-    echo "Creating frpc.ini done"
+    echo "Creating frpc.toml done"
 fi
 
 
-# Start FRPC with command: ./frpc -c frpc.ini
+# Start FRPC with command: ./frpc -c frpc.toml
 echo "Starting FRP client..."
 
-cat /usr/src/frpc.ini
+cat /usr/src/frpc.toml
 
-exec ./usr/src/frpc -c /usr/src/frpc.ini
+exec ./usr/src/frpc -c /usr/src/frpc.toml
 
 trap "stop_frpc" SIGTERM SIGHUP
 
